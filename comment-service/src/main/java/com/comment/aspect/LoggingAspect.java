6c864c44e297f1969a6b7ee410a04657d252dcf4
jayanthvvo <jayanthvvo395@gmail.com>
2026-04-08 14:31:14 +0530

    logging implemented across all services

--- comment-service/src/main/java/com/comment/aspect/LoggingAspect.java
+++ comment-service/src/main/java/com/comment/aspect/LoggingAspect.java
@@ -1,15 +1,9 @@
-package com.example.article.aspect;
+package com.comment.aspect;
 
 import java.util.Arrays;
-
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
-
 import lombok.extern.slf4j.Slf4j;
 
 @Aspect
@@ -17,29 +11,25 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class LoggingAspect {
 	
-	
-	@Pointcut("execution(* com.example.article.controller.*.*(..))")
+	@Pointcut("execution(* com.comment.controller.*.*(..))")
 	public void controllerMethods() {}
 
 	@Before("controllerMethods()")
-	public void logbeforemethod(JoinPoint joinPoint) {
+	public void logBeforeMethod(JoinPoint joinPoint) {
 		String methodName = joinPoint.getSignature().getName();
         String className = joinPoint.getTarget().getClass().getSimpleName();
         Object[] args = joinPoint.getArgs();
-        
         log.info("➡️ [ENTER] {}::{}() with arguments: {}", className, methodName, Arrays.toString(args));
 	}
 	
 	@AfterReturning(pointcut = "controllerMethods()", returning = "result")
-	public void logaftermethod(JoinPoint joinPoint, Object result) {
+	public void logAfterMethod(JoinPoint joinPoint, Object result) {
         String methodName = joinPoint.getSignature().getName();
-        
         log.info("✅ [EXIT] {}() returned: {}", methodName, result != null ? result.toString() : "null");
 	}
 	
-	
 	@AfterThrowing(pointcut = "controllerMethods()", throwing = "exception")
-	public void logifexception(JoinPoint joinPoint, Throwable exception) {
+	public void logIfException(JoinPoint joinPoint, Throwable exception) {
 		String methodName = joinPoint.getSignature().getName();
 		log.error("❌ [ERROR] {}() crashed with exception: {}", methodName, exception.getMessage());
 	}
